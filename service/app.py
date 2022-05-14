53d7f4de4b2907514b81693844fe3e960108cf8d
RomanZorkin <zorkin.roman@gmail.com>
2022-05-14 11:33:49 +0700

    linters control 1

--- service/app.py
+++ service/app.py
@@ -1,9 +1,9 @@
-from asyncio.log import logger
 import logging
-import os
+from re import A
 import time
-import sys
 from pathlib import Path
+from typing import Any
+
 
 import cv2
 import numpy as np
@@ -15,67 +15,52 @@ SCORE_THRESHOLD = 0.5
 IOU_THRESHOLD = 0.5
 
 # конфигурация нейронной сети
-config_path = 'service/cfg/yolov3.cfg'
+config_path = Path('service/cfg/yolov3.cfg')
 # файл весов сети YOLO
-weights_path = 'service/weights/yolov3.weights'
-# weights_path = "weights/yolov3-tiny.weights"
+weights_path = Path('service/weights/yolov3.weights')
 # файл всех меток классов (объектов)
 labels_path = Path('service/data/coco.names')
 # файл изображения
-image_path = 'service/images/morj_2.jpg'
+image_path = Path('service/images/morj_2.jpg')
+filename, ext = image_path.stem, image_path.suffix
 
 # загрузка всех меток классов (объектов)
-logg.debug(Path.cwd())
-
 with open(labels_path, 'r') as f:
     labels = f.read().strip().split('\n')
-
-
 # генерируем цвета для каждого объекта и последующего построения
 colors = np.random.randint(0, 255, size=(len(labels), 3), dtype='uint8')
 
-# загружаем сеть YOLO
-# документация https://docs.opencv.org/3.4/d6/d0f/group__dnn.html#gaef8ac647296804e79d463d0e14af8e9d
-net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
 
-# подготовка изображения
-image = cv2.imread(image_path)
-image_file_basename = os.path.basename(image_path)
-filename, ext = image_file_basename.split('.')
-
-# Затем нам нужно нормализовать, масштабировать и изменить это изображение
-h, w = image.shape[:2]
+def image_prepare(image_path: Path) -> cv2:
+    # подготовка изображения
+    return cv2.imread(str(image_path))
 
 
-def create_layer(image: cv2) -> cv2:
+def layer_outputs(image: cv2) -> cv2:
+    # загружаем сеть YOLO
+    # документация https://docs.opencv.org/3.4/d6/d0f/group__dnn.html#gaef8ac647296804e79d463d0e14af8e9d
+    net = cv2.dnn.readNetFromDarknet(str(config_path), str(weights_path))
     # прогнозирование
     ####################################################################
     # создать 4D blob
-    blob = cv2.dnn.blobFromImage(image, 1/255.0, (416, 416), swapRB=True, crop=False)
-
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
     logg.debug(f'image.shape: {image.shape}, blob.shape {blob.shape}')
     # усанавливаем blob как вход сети
     net.setInput(blob)
     # получаем имена всех слоев
     ln = net.getLayerNames()
-    # здесь исправил от исходного было ln[i[0] - 1]
     ln = [ln[i - 1] for i in net.getUnconnectedOutLayers()]
     # прямая связь (вывод) и получение выхода сети
     # измерение времени для обработки в секундах
-    start = time.perf_counter()
-    layer_outputs = net.forward(ln)
-    delta_time = time.perf_counter() - start
-    logg.debug(f'Потребовалось: {delta_time:.2f}s')
-    return layer_outputs
+    return net.forward(ln)
 
 
-def create_entities(image: cv2) -> list:
+def create_entities(image: cv2, h: int, w: int) -> tuple[list[Any], list[Any], list[Any]]:
     # еребрать выходные данные нейронной сети и отбросить все объекты, уровень достоверности
     # идентификации которых меньше, чем параметр CONFIDENCE
-    layer_outputs = create_layer(image)
     boxes, confidences, class_ids = [], [], []
     # перебираем каждый выход слоя
-    for output in layer_outputs:
+    for output in layer_outputs(image):
         # перебираем каждое обнаружение объекта
         for detection in output:
             # извлекаем идентификатор класса (метку) и достоверность (как вероятность)
@@ -103,15 +88,15 @@ def create_entities(image: cv2) -> list:
                 class_ids.append(class_id)
 
     logg.info(f'detection.shape: {detection.shape}')
-    return boxes, confidences, class_ids
+    return (boxes, confidences, class_ids)
 
-# Отрисовка обнаруженных объектов
-####################################################################
-# перебираем сохраняемые индексы
+
 def render_image(idxs: cv2, image: cv2, boxes, confidences, class_ids) -> None:
+    # Отрисовка обнаруженных объектов
+    ####################################################################
+    # перебираем сохраняемые индексы
     font_scale = 1
     thickness = 1
-
 
     for i in idxs.flatten():
         # извлекаем координаты ограничивающего прямоугольника
@@ -143,17 +128,22 @@ def render_image(idxs: cv2, image: cv2, boxes, confidences, class_ids) -> None:
             color=(0, 0, 0),
             thickness=thickness,
         )
-
-    cv2.imwrite('service/recognized/' + filename + '_yolov3.' + ext, image)
-
-
-# Non-maximal Suppression
-####################################################################
-# выполнить не максимальное подавление с учетом оценок, определенных ранее
+    new_image_path = f'service/recognized/{filename}_yolov3{ext}'
+    cv2.imwrite(new_image_path, image)
 
 
 def recognize():
-    boxes, confidences, class_ids = create_entities(image)
+    start = time.perf_counter()
+    image = image_prepare(image_path)
+    # Затем нам нужно нормализовать, масштабировать и изменить это изображение
+    height, width = image.shape[:2]
+    logg.debug(f'height {height}, width{width}')
+    boxes, confidences, class_ids = create_entities(image, height, width)
+    # Non-maximal Suppression
+    ####################################################################
+    # выполнить не максимальное подавление с учетом оценок, определенных ранее
     idxs = cv2.dnn.NMSBoxes(boxes, confidences, SCORE_THRESHOLD, IOU_THRESHOLD)
     if len(idxs) > 0:
         render_image(idxs, image, boxes, confidences, class_ids)
+    delta_time = time.perf_counter() - start
+    logg.debug(f'Потребовалось: {delta_time:.2f}s')
