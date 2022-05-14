f5d7eb837339be9a35564e026ff551fe14e8d0bc
RomanZorkin <zorkin.roman@gmail.com>
2022-05-14 15:26:10 +0700

    simple work

--- service/__main__.py
+++ service/__main__.py
@@ -5,5 +5,5 @@ from service import app, app_torch
 logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == '__main__':
-    #app.recognize()
-    app_torch.run()
+    app.recognize()
+    #app_torch.run()
