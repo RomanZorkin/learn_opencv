21ab56225b08a938626e6f5b701731b047645361
RomanZorkin <zorkin.roman@gmail.com>
2022-05-15 21:21:16 +0700

    new recognizer service

---
+++ recognizer/app.py
@@ -0,0 +1,14 @@
+import logging
+from pathlib import Path
+
+import cv2
+
+logger = logging.getLogger(__name__)
+
+model_net = Path('recognizer/models/cat_dogs.pth')
+
+def create_net(net_path: Path):
+    
+
+def run():
+    logger.debug('start _run function')
